624104a7c0da1420784a705e92beda2324e9a5c0
Yuji Shinozaki <ys2n@virginia.edu>
2011-07-28 10:06:48 -0400

    Refactored BridgeToolPermissionEvaluator.hasPermission()

--- src/main/java/edu/virginia/shanti/om/bridge/auth/BridgeToolPermissionEvaluator.java
+++ src/main/java/edu/virginia/shanti/om/bridge/auth/BridgeToolPermissionEvaluator.java
@@ -16,61 +16,77 @@ import org.springframework.security.core.GrantedAuthority;
 import edu.virginia.shanti.om.bridge.domain.Bridge;
 
 public class BridgeToolPermissionEvaluator implements PermissionEvaluator {
-	
-	
+
 	Logger log = LoggerFactory.getLogger(BridgeToolPermissionEvaluator.class);
-//	Log log = LogFactory.getLog(BridgeToolPermissionEvaluator.class);
+
+	// Log log = LogFactory.getLog(BridgeToolPermissionEvaluator.class);
 
 	@Override
 	public boolean hasPermission(Authentication authentication,
 			Object targetDomainObject, Object permission) {
 		// TODO Auto-generated method stub
-		log.warn("hasPermission called by " + authentication + " on " + targetDomainObject + " for permission " + permission);
-		
+		log.warn("hasPermission called by " + authentication + " on "
+				+ targetDomainObject + " for permission " + permission);
+
 		log.warn("Authorities: " + authentication.getAuthorities());
-		
-		Collection<GrantedAuthority> auths = (Collection<GrantedAuthority>) authentication.getAuthorities();
+
+		Collection<GrantedAuthority> auths = (Collection<GrantedAuthority>) authentication
+				.getAuthorities();
 
 		boolean authorized = false;
-		
-		if (!auths.isEmpty() && targetDomainObject instanceof Bridge) {
-			Bridge bridge = (Bridge)targetDomainObject;
+
+		if (!auths.isEmpty()) {
+			String localContext;
+			if (targetDomainObject instanceof Bridge) {
+				Bridge bridge = (Bridge) targetDomainObject;
+				localContext = bridge.getLocalContext();
+			} else if (targetDomainObject instanceof String) {
+				localContext = (String)targetDomainObject;
+			}
+			else {
+				log.warn("Object was an unrecognized class: " + targetDomainObject.getClass().toString());
+				return false;
+			}
 			for (GrantedAuthority grant : auths) {
-				if (isAdmin(grant.getAuthority(),bridge.getLocalContext())) {
+				if (isAdmin(grant.getAuthority(), localContext)) {
 					authorized = true;
 					break;
 				}
 			}
+			
 		}
-		
+
 		return authorized;
 	}
-	
+
 	private boolean isAdmin(String authority, String localContext) {
-		List<String> adminRoles = Arrays.asList(new String[] {"Instructor", "Secondary Instructor", "Owner", "maintain", "Administrator"});
+		List<String> adminRoles = Arrays.asList(new String[] { "Instructor",
+				"Secondary Instructor", "Owner", "maintain", "Administrator" });
 
 		String[] parts = authority.split("@");
-		
+
 		if (parts.length != 2) {
-			throw new IllegalArgumentException("authority must include at \'@\' symbol");
+			throw new IllegalArgumentException(
+					"authority must include at \'@\' symbol");
 		}
-		
+
 		String role = parts[0];
 		String glc = parts[1];
-		
+
 		boolean isAdminRole = adminRoles.contains(role);
 		boolean isMatchingContext = glc.equals(localContext);
-		
-		log.error("Checking localContext " + localContext + " is the same as granted local context " + glc + ": " + isMatchingContext);
-		log.error("Checking role " + role + " is contained in " + adminRoles + ": " + isAdminRole);
-	
-		
+
+		log.error("Checking localContext " + localContext
+				+ " is the same as granted local context " + glc + ": "
+				+ isMatchingContext);
+		log.error("Checking role " + role + " is contained in " + adminRoles
+				+ ": " + isAdminRole);
+
 		boolean check = isMatchingContext && isAdminRole;
-		
+
 		System.err.println("===> Therefore check is: " + check);
-		
+
 		return check;
-		
 
 	}
 
@@ -78,7 +94,9 @@ public class BridgeToolPermissionEvaluator implements PermissionEvaluator {
 	public boolean hasPermission(Authentication authentication,
 			Serializable targetId, String targetType, Object permission) {
 		// TODO Auto-generated method stub
-		log.warn("hasPermission called by " + authentication + " on " + targetId + " of type " + targetType + " for permission " + permission);
+		log.warn("hasPermission called by " + authentication + " on "
+				+ targetId + " of type " + targetType + " for permission "
+				+ permission);
 		return false;
 	}
 
---
+++ src/main/java/edu/virginia/shanti/om/bridge/auth/BridgeToolPermissionEvaluatorTest.java
@@ -0,0 +1,94 @@
+package edu.virginia.shanti.om.bridge.auth;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import edu.virginia.shanti.om.bridge.domain.Bridge;
+import edu.virginia.shanti.om.bridge.service.CurrentUser;
+import edu.virginia.shanti.om.bridge.service.CurrentUserImpl;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/META-INF/spring/applicationContext.xml", "/META-INF/spring/applicationContext-security.xml" })
+public class BridgeToolPermissionEvaluatorTest {
+	
+	private static final Object USER = null;
+	private static final Object CREDS = null;
+	
+	@Autowired
+	private BridgeToolPermissionEvaluator btpe;
+	
+	@Autowired
+	private CurrentUserImpl currentUser;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		Collection<GrantedAuthority> grants = new HashSet<GrantedAuthority>();
+		
+		grants.add(new GrantedAuthorityImpl("Student@blahblahblah"));
+		grants.add(new GrantedAuthorityImpl("Instructor@yeahyeahyeah"));
+		grants.add(new GrantedAuthorityImpl("Administrator@whoawhoawhoa"));
+		grants.add(new GrantedAuthorityImpl("Member@sososo"));
+		
+		Authentication auth = new UsernamePasswordAuthenticationToken(USER, CREDS, grants);
+				
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		
+	}
+	
+	@Test
+	public void injected() {
+		
+		assertNotNull("BridgeToolPermissionEvaluator was not injected",btpe);
+		
+	}
+	
+	
+
+	@Test
+	public void testHasPermissionAuthenticationObjectObject() {
+		
+		Bridge bridge = new Bridge();
+		bridge.setLocalContext("yeahyeahyeah");
+		bridge.setCurrentUser(currentUser);
+		bridge.setBridgeToolPermissionEvaluator(btpe);
+
+		assertTrue(btpe.hasPermission(currentUser.getAuthentication(), bridge, "admin"));
+	
+	}
+	
+	@Test
+	public void testBridgeIsAdmin() {
+		
+		Bridge bridge = new Bridge();
+		bridge.setLocalContext("blahblahblah");
+		bridge.setCurrentUser(currentUser);
+		assertFalse(bridge.isAdmin());
+		
+		bridge.setLocalContext("whoawhoawhoa");
+		assertTrue(bridge.isAdmin());
+		
+	}
+
+	@Test
+	public void testHasPermissionAuthenticationSerializableStringObject() {
+		fail("Not yet implemented");
+	}
+
+}
