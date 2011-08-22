4e6f109da6471c831e714dbc3bc80e785ebdaaa4
Yuji Shinozaki <ys2n@virginia.edu>
2011-08-22 15:18:09 -0400

    Introduced SiteAliasMappings

---
+++ src/main/java/edu/virginia/shanti/om/bridge/util/SiteAliasMapping.java
@@ -0,0 +1,19 @@
+package edu.virginia.shanti.om.bridge.util;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+
+@RooJavaBean
+public class SiteAliasMapping {
+	
+	
+	public SiteAliasMapping(String remoteName, String localContext, String alias) {
+		this.remoteName = remoteName;
+		this.localContext = localContext;
+		this.alias = alias;		
+	}
+	
+	String remoteName;
+	String localContext;
+	String alias;
+
+}
--- src/main/java/edu/virginia/shanti/om/bridge/web/SiteAliasController.java
+++ src/main/java/edu/virginia/shanti/om/bridge/web/SiteAliasController.java
@@ -1,5 +1,8 @@
 package edu.virginia.shanti.om.bridge.web;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.virginia.shanti.om.bridge.domain.SiteAlias;
+import edu.virginia.shanti.om.bridge.util.SiteAliasMapping;
 import flexjson.JSONSerializer;
 
 @RooWebScaffold(path = "sitealiases", formBackingObject = SiteAlias.class)
@@ -18,13 +22,27 @@ import flexjson.JSONSerializer;
 public class SiteAliasController {
 
 	@RequestMapping(value = "/listMappings", method = RequestMethod.GET)
-    @ResponseBody
+	@ResponseBody
 	public Object listAliasServiceMappings() {
 		EntityManager em = SiteAlias.entityManager();
 		Query q = em
-				.createQuery("SELECT b.remoteName, b.localContext, b.siteAlias.alias as alias FROM Bridge b join b.siteAlias");
-		return new JSONSerializer().exclude("*.class").serialize(
-				q.getResultList());
+				.createQuery(
+						"SELECT b.remoteName, b.localContext, b.siteAlias.alias as alias FROM Bridge b join b.siteAlias");
+
+		List resultList = q.getResultList();
+
+		List<SiteAliasMapping> mappings = new LinkedList<SiteAliasMapping>();
+		for (Object resultRow : resultList) {
+
+			String remoteName = (String) ((Object[]) resultRow)[0];
+			String localContext = (String) ((Object[]) resultRow)[1];
+			String alias = (String) ((Object[]) resultRow)[2];
+
+			mappings.add(new SiteAliasMapping(remoteName, localContext, alias));
+
+		}
+
+		return new JSONSerializer().exclude("*.class").serialize(mappings);
 	}
 
 }
