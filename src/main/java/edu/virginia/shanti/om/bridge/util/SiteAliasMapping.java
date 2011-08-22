38855b640c22edfbcfd3acdc7866b8a30b72603a
Yuji Shinozaki <ys2n@virginia.edu>
2011-08-22 16:51:44 -0400

    Fixed hashCode and equals to implement comparison via value in SiteAliasMapping.

--- src/main/java/edu/virginia/shanti/om/bridge/util/SiteAliasMapping.java
+++ src/main/java/edu/virginia/shanti/om/bridge/util/SiteAliasMapping.java
@@ -15,5 +15,13 @@ public class SiteAliasMapping {
 	String remoteName;
 	String localContext;
 	String alias;
+	@Override
+	public boolean equals(Object obj) {
+		return this.hashCode() == obj.hashCode();
+	}
+	@Override
+	public int hashCode() {
+		return toString().hashCode();
+	}
 
 }
