e51d9acccfd344bdd28da2fcd01d14c7739bf8a1
ys2n <ys2n@virginia.edu>
2017-05-24 05:03:51 -0400

    using sakaisession instead of truncated session

--- src/main/java/edu/virginia/shanti/om/bridge/service/SitePropertyService.java
+++ src/main/java/edu/virginia/shanti/om/bridge/service/SitePropertyService.java
@@ -72,17 +72,20 @@ public class SitePropertyService {
 		.getAuthentication().getCredentials();
 
 		String[] split = sakaisession.split("\\.");
-		String session = split[0];
+		// String session = split[0];
+		String session = sakaisession;
 		SessionAffinity aff = SessionAffinityUtility.constructSessionAffinity(currentUser);
 		String server = aff.getServer(); 
-		String paramDebug = "DEBUG INFO: \n\tsession = " + session
+		String paramDebug = "DEBUG INFO: "
+				+ "\n\taffinity = " + aff
+				+ "\n\tsession = " + session
 				+ "\n\tserver = " + server
-				+ "\n\tadminsecret = (" + adminsecret.length() + " chars) "
+//				+ "\n\tadminsecret = (" + adminsecret.length() + " chars) "
+				+ "\n\tadminsecret = " + adminsecret
 				+ "\n\tsiteId = " + siteId
 				+ "\n\tpropertyname = " + propertyName
 				+ "\n\tvalue = " + value
-				+ "\n\tsakaisession = " + sakaisession
-				+ "\n\taffinity = " + aff;
+				+ "\n\tsakaisession = " + sakaisession;
 		log.info(paramDebug);
 		try {
 			SakaiScript_PortType sakaiScript = sakaiScriptServiceLocator
@@ -96,7 +99,9 @@ public class SitePropertyService {
 		} catch (ServiceException e) {
 			throw new RuntimeException("service failure: " + paramDebug, e);
 		} catch (RemoteException e) {
-			throw new RuntimeException("remote failure: " + paramDebug, e);
+			
+			
+			throw new RuntimeException("remote failure: " + e.getMessage() + " "+ paramDebug, e);
 		} catch (MalformedURLException e) {
 			throw new RuntimeException("url problem: " + paramDebug, e);
 		}
